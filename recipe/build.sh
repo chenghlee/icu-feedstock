bc3998c7a2e3d94ff08cbc80512d292cb6b4be0b
Ray Donnelly <mingw.android@gmail.com>
2017-08-26 21:11:12 +0100

    Update to 58.2 and generalized pinnings

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,22 +5,22 @@ cd source
 chmod +x configure install-sh
 
 EXTRA_OPTS=""
-if [ "$(uname)" == "Darwin" ]; then
+if [[ ${HOST} =~ .*darwin.* ]]; then
   EXTRA_OPTS="--enable-rpath"
 fi
 
-./configure --prefix="$PREFIX" \
-            --build=$BUILD \
-            --host=$HOST \
-            --disable-samples \
-            --disable-extras \
-            --disable-layout \
-            --disable-tests \
-            --enable-static \
+./configure --prefix="${PREFIX}"  \
+            --build=${BUILD}      \
+            --host=${HOST}        \
+            --disable-samples     \
+            --disable-extras      \
+            --disable-layout      \
+            --disable-tests       \
+            --enable-static       \
             "${EXTRA_OPTS}"
 
-make -j$CPU_COUNT
+make -j${CPU_COUNT} VERBOSE=1
 make check
 make install
 
-rm -rf $PREFIX/sbin
+rm -rf ${PREFIX}/sbin
